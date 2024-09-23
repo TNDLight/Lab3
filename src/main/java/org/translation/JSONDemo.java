9ce9293fcf9772040faec2422aff601d6e0b7b3b
Anthony Sun <tndlight@Anthonys-MacBook-Air-5.local>
2024-09-23 17:31:52 -0400

    JSONDemo upload

--- src/main/java/org/translation/JSONDemo.java
+++ src/main/java/org/translation/JSONDemo.java
@@ -34,7 +34,8 @@ public class JSONDemo {
      */
     public static String getKeyOneOfSecond(JSONArray jsonArray) {
         // TODO: Complete this method.
-        return "";
+        JSONObject second_obj = jsonArray.getJSONObject(1);
+        return second_obj.getString("key1");
     }
 
 }
